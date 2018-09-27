eebd81ddd071c3668def6b48e1b55a59912477ac
thaisluca <thaisluca@uol.com.br>
2018-09-27 18:55:37 -0300

    Evitando anagramas repetidos,

--- anagramas.py
+++ anagramas.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 
+# Autor: Thais Luca
+# Tópicos Especiais em Algoritmos - Departamento de Ciência da Computação, UFRJ
+# 26/09/2018
+
 import numpy as np
 import collections
 
 vogais = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
-letras_disponiveis = {}
+letras_disponiveis = {}     # guarda todas as letras e o número de ocorrências de cada letra na palavra original.
+
+anagramasEncontrados = {}   # dicionário para guardar os anagramas que já saíram e evitar 
+                            # que anagramas de palavras como "ana" e "ovo" tenham o mesmo anagrama
+                            # exibido quatro vezes.
 
 
 def backtrack(anagrama, tamanho_anagrama, letras):
@@ -12,7 +20,10 @@ def backtrack(anagrama, tamanho_anagrama, letras):
     # 1. verifique se o estado corrente merece tratamento especial
     #  (se é um estado "final")
     if len(anagrama) == tamanho_anagrama:
-        print anagrama
+        str1 = ''.join([str(i) for i in anagrama])  # Dicionários não permitem arrays como chaves, então
+        if str1 not in anagramasEncontrados:        # primeiro converto para string e verifico se o anagrama
+            print str1                              # já foi impresso.
+            anagramasEncontrados[str1] = 1
         return True  # encontrei!
 
     
@@ -24,16 +35,16 @@ def backtrack(anagrama, tamanho_anagrama, letras):
             continue       # a primeira letra do anagrama deve ser uma vogal!
 
         if candidato in anagrama and anagrama.count(candidato) == letras_disponiveis[candidato]: 
-            continue    # o número de ocorrências de uma letra no anagrama deve ser o mesmo da palavra!
+            continue    # o número de ocorrências de uma letra no anagrama deve ser o mesmo da palavra original!
 
         if candidato == 'g' and 'p' in letras_disponiveis and 'p' not in anagrama:
-            continue  # p deve aparecer primeiro que g!
+            continue  # se a palavra original contém a letra p, esta deve aparecer primeiro que a letra g!
 
         if candidato not in vogais and len(anagrama) > 2 and verificaSeAsDuasUltimasLetrasSaoConsoantes(anagrama):
             continue  # não podemos ter duas três consoantes consecutivas!
 
         if len(anagrama) > 0 and candidato == anagrama[-1]:
-           continue  # essa letra é repetitida, não posso usá-la!
+           continue  # essa letra é igual a última letra, não posso ter duas letras consecutivas!
 
         # 2.2 modifica o anagrama usando o candidato
         anagrama.append(candidato) 
