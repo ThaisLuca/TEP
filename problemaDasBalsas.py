429fa36cf87ec3b07c9262b3c645ee7cb8c9584f
thaisluca <thaisluca@uol.com.br>
2018-10-08 22:19:21 -0300

    Problema sem repetir saída

--- problemaDasBalsas.py
+++ problemaDasBalsas.py
@@ -9,33 +9,35 @@
 ESQUERDA = 'E'
 DIREITA = 'D'
 
-import sys
-
 encontreiSolucaoOtima = False
+solucaoOtima = None
 
 def backtrack(configuracao, ladoEsquerdo, ladoDireito, carros):
-    global encontreiSolucaoOtima
+    global encontreiSolucaoOtima, solucaoOtima
 
     if(ladoEsquerdo < 0 or ladoDireito < 0):  
         return
 
+    if len(configuracao) != len(carros):
+        proximoDaFila = carros[len(configuracao)]
+
+        if(proximoDaFila > ladoEsquerdo and  proximoDaFila > ladoDireito):
+            return
+
+
     if(encontreiSolucaoOtima):
-        #configuracao.pop()
-        print configuracao
         return
 
     if(len(configuracao) == len(carros)):
         encontreiSolucaoOtima = True
+        solucaoOtima = configuracao
         return
 
     proximoDaFila = carros[len(configuracao)]
-
     espacoLivre = ladoDireito + ladoEsquerdo
     if(espacoLivre < proximoDaFila):
         encontreiSolucaoOtima = True
-        return
-
-    if(proximoDaFila > ladoEsquerdo and  proximoDaFila > ladoDireito):
+        solucaoOtima = configuracao
         return
 
     configuracao.append(ESQUERDA)
@@ -57,6 +59,8 @@ def balsas(L, carros):
     ladoDireito = L     #Tamanho do corredor da direita
 
     backtrack(configuracao, ladoEsquerdo, ladoDireito, carros)
+    solucaoOtima.pop()
+    print solucaoOtima
 
 
 balsas(100, [40, 35, 20, 60, 30, 12, 18])
