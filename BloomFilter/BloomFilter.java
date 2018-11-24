7f9d3f12a0e91037a341634b45dc6d928db76bfa
ThaisLuca <thaisluca@uol.com.br>
2018-11-24 21:52:45 -0200

    Término Bloom Filter

--- BloomFilter/BloomFilter.java
+++ BloomFilter/BloomFilter.java
@@ -1,67 +1,86 @@
-import java.util.*;
-import java.lang.Long;
-import sun.nio.*;
+/**
+ * Autor: Thais Luca
+ * Tópicos Especiais em Programação - Departamento de Ciência da Computação, UFRJ
+ * 23/11/18
+ */
 
+/**
+ * Classe que representa um Bloom Filter
+ */
 public class BloomFilter {
-	private boolean[] espacos;
-	private int tamanho;
-	private int hash;
+    private boolean[] espacos;
+    private int tamanho;
+    private int hash;
 
+    //Construtor
+    public BloomFilter(int tamanho, int hash) {
+        this.tamanho = tamanho;
+        this.hash = hash;
+        this.espacos = new boolean[tamanho];
+    }
 
-	//Construtor
-	public BloomFilter(int tamanho, int hash) {
-		this.tamanho = tamanho;
-		this.hash = hash;
-		this.espacos = new boolean[tamanho];
-	}
+    /**
+     * Verifica se o filtro está vazio
+     * @return True se o filtro está vazio, False caso contrário
+     */
+    public boolean filtroVazio() {
+        return tamanho == 0;
+    }
 
- 
-	public boolean filtroVazio() {
-		return tamanho == 0;
-	}
+    /**
+     * Retorna o tamanho do filtro
+     * @return tamanho do filtro em bytes
+     */
+    public int getTamanho() {
+        return tamanho;
+    }
 
-	public int getTamanho() {
-		return tamanho;
-	}
+    /**
+     * Adiciona um termo ao filtro
+     * @param termo o termo a ser adicionado
+     */
+    public void adicionarTermo(Long termo){
+        int[] res = calcularEspacos(termo);
+        for(int e : res){
+            this.espacos[e] = true;
+        }
+    }
 
-	public void adicionarTermo(long termo){
-		int[] res = calcularEspacos(termo);
-		for(int e : res){
-			this.espacos[e] = true;
-		}
-	}
+    /**
+     * Verifica se um termo está no filtro
+     * @param termo o termo a ser pesquisado
+     * @return True se o termo está no filtro, False caso contrário
+     */
+    public boolean buscarTermo(long termo){
+        int[] res = calcularEspacos(termo);
+        for(int esp : res){
+            if(!this.espacos[esp]){
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public boolean buscarTermo(Long termo){
-		int[] res = calcularEspacos(termo);
-		for(int esp : res){
-			if(!this.espacos[esp]){
-				return false;
-			}
-		}
-		return true;
-	}
+    /**
+     * Calcula as posições para inserir o termo
+     * @param termo o termo a ser inserido
+     * @return o vetor contendo os endereços
+     */
+    private int[] calcularEspacos(long termo){
+        int[] esp = new int[hash];
+        for(int i = 0; i < hash; i++){
+            esp[i] = calcularHash(termo, i);
+        }
+        return esp;
+    }
 
-	public static void main(String[] args){
-		System.out.println("Oi");
-		BloomFilter f = new BloomFilter(4, 2);
-		f.adicionarTermo(2);
-		if(!f.filtroVazio()){
-			System.out.println("Vazio");
-		}
-		System.out.println(f.buscarTermo((long) 2));
-	}
-
-	private int[] calcularEspacos(long termo){
-		int[] esp = new int[hash];
-		for(int i = 0; i < hash; i++){
-			esp[i] = calcularHash(termo, i);
-		}
-		return esp;
-	}
-
-	private int calcularHash(long termo, int i){
-		return Math.abs(Long.valueOf(termo*i).hashCode()) % this.tamanho;
-	}
-
-
+    /**
+     * Função hash para cálculo das posições de inserção
+     * @param termo o termo a ser inserido
+     * @param i o índice do termo na lista
+     * @return o espaço para adicionar uma parte do termo
+     */
+    private int calcularHash(long termo, int i){
+        return Math.abs(Long.valueOf(termo*i).hashCode()) % this.tamanho;
+    }
 }
--- BloomFilter/BloomFilterTest.java
+++ BloomFilter/BloomFilterTest.java
@@ -1,32 +1,102 @@
 import java.util.*;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+/**
+ * Autor: Thais Luca
+ * Tópicos Especiais em Programação - Departamento de Ciência da Computação, UFRJ
+ * 23/11/18
+ */
+
+/**
+ * Classe para testar o Bloom Filter
+ */
+@RunWith(JUnit4.class)
 public class BloomFilterTest {
 
-	private BloomFilter filtro;
-	private static int NUMERO_TERMOS = 2000;
-	private static int TAMANHO_MAX = 1000;
+    private static ArrayList<Long> termos;
+    private BloomFilter filtro;
+    private static int NUMERO_TERMOS = 2000;
+    private static int TAMANHO_MAX = 10000;
 
-	@BeforeClass
-	public void setUpClass(){
-		Random random = new Random(100);
-		Set<Long> termos = new HashSet<Long>();
-		for(int i = 0; i < NUMERO_TERMOS; i++){
-			termos.add((long) random.nextInt(TAMANHO_MAX));
-		}
-	}
+    @BeforeClass
+    public static void setUpClass(){
+        termos =  new ArrayList<Long>();
+        Random random = new Random(100);
+        for(int i = 0; i < NUMERO_TERMOS; i++){
+            termos.add((long) random.nextInt(TAMANHO_MAX));
+        }
+    }
 
-	@Before
-	public void setUp(){
-		filtro = new BloomFilter(4 * NUMERO_TERMOS, 4);
-	}
+    @Before
+    public void setUp(){
+        filtro = new BloomFilter(4 * NUMERO_TERMOS, 2);
+    }
 
-	@Test 
-	public void testarAdicionarNovosTermos(){
-		for(long t: termos){
-			filtro.adicionarTermo(t);
-		}
-		assertFalse(filtro.filtroVazio());
-	}
+    @Test
+    public void testarAdicionarNovosTermos(){
+        for(long t: termos){
+            filtro.adicionarTermo(t);
+        }
+        assertFalse(filtro.filtroVazio());
+    }
+
+    @Test
+    public void testarVerdadeiroPositivo(){
+        filtro.adicionarTermo((long) 324);
+        assertTrue(filtro.buscarTermo((long) 324));
+    }
+
+    @Test
+    public void testarVerdadeiroNegativo(){
+        assertFalse(filtro.buscarTermo(TAMANHO_MAX + 1));
+    }
+
+    @Test
+    public void testarLista(){
+        int termosEncontrados = 0;
+        int falsosPositivos = 0;
+        int falsosNegativos = 0;
+        int verdadeirosPositivos = 0;
+        int verdadeirosNegativos = 0;
+
+        adicionarTermos();
+
+        for(long i = 0; i < TAMANHO_MAX; i++){
+            boolean termoEncontrado = filtro.buscarTermo(i);
+            if(termoEncontrado){
+                termosEncontrados++;
+            }
+            if(termos.contains(i)){
+                if(!termoEncontrado) {
+                    falsosNegativos++;
+                } else {
+                    verdadeirosPositivos++;
+                }
+            } else {
+                if(termoEncontrado) {
+                    falsosPositivos++;
+                }
+                else {
+                    verdadeirosNegativos++;
+                }
+            }
+        }
+
+        System.out.println("Número de termos encontrados: " + termosEncontrados);
+        System.out.println("Número de verdadeiros positivos: " + verdadeirosPositivos);
+        System.out.println("Número de verdadeiros negativos: " + verdadeirosNegativos);
+        System.out.println("Número de falsos positivos: " + falsosPositivos);
+        System.out.println("Número de falsos negativos: " + falsosNegativos);
+    }
+
+    private void adicionarTermos(){
+        for(long t: termos){
+            filtro.adicionarTermo(t);
+        }
+    }
 }
